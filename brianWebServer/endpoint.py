39d467a4fbf72a42b96ea7808fe8d16496576c49
twardmat <mateusz.p.twardowski@gmail.com>
2023-01-18 20:28:30 +0100

    endpoint configuration

--- brianWebServer/endpoint.py
+++ brianWebServer/endpoint.py
@@ -1,1 +1,14 @@
-abs
+from flask import Flask
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def hello_world():
+    return "hello motherfuckers"
+
+
+if __name__ == "__main__":
+    print("Run main")
+    app.run(host="0.0.0.0", port=80)
